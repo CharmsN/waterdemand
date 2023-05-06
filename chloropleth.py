744530e74176900992d3c782995279111dfd7d95
CharmsN <Newmarch-C@Ulster.ac.uk>
2023-05-06 20:10:44 +0100

    refinements

--- chloropleth.py
+++ chloropleth.py
@@ -32,32 +32,6 @@ def chloropleth_pcc(pcc_period):
     # wrz.head(10)
     myFig = plt.figure(figsize=(10, 10))  # create a figure of size 10x10 (representing the page size in inches)
     myCRS = ccrs.TransverseMercator(27700)  # create a Universal Transverse Mercator reference system to transform our data.
-   
-    # Uncomment and remove indent from this section this to see plot of water company boundaries
-        # ax = plt.axes(projection=myCRS)  # finally, create an axes object in the figure, using a UTM projection,
-        #outline_feature = ShapelyFeature(outline['geometry'], myCRS, edgecolor='k', facecolor='w') # first, we just add the outline using cartopy's ShapelyFeature
-        #xmin, ymin, xmax, ymax = outline.total_bounds
-        #ax.add_feature(outline_feature) # add the features we've created to the map. 
-       # ax.set_extent([xmin-5000, xmax+5000, ymin-5000, ymax+5000], crs=myCRS) 
-        #myFig ## re-draw the figure
-        #num_wrz = len(wrz.COMPANY.unique())  # get the number of unique water companies we have in the dataset
-        # print('Number of unique features: {}'.format(num_wrz))
-       # num_colours = num_wrz  # pick colours for the individual water companies- generate random RGB colors# Number of colours to generate
-       # company_colours = sns.color_palette("husl", n_colors=num_colours)  # Use Seaborn color palette
-       # company_names = list(wrz.COMPANY.unique())  # get a list of unique names for the company boundaries
-      #  company_names.sort() # sort the companies alphabetically by name
-        # next, add the company outlines to the map using the colours that we've picked.
-      #  for ii, name in enumerate(company_names):
-      #      feat = ShapelyFeature(wrz.loc[wrz['COMPANY'] == name, 'geometry'], # first argument is the geometry
-       #                           myCRS, # second argument is the CRS
-       #                           edgecolor='k', # outline the feature in black
-       #                           facecolor=company_colours[ii], # set the face color to the corresponding color from the list
-       #                           linewidth=1, # set the outline width to be 1 pt
-       #                           alpha=0.25) # set the alpha (transparency) to be 0.25 (out of 1)
-       #     ax.add_feature(feat) # once we have created the feature, we have to add it to the map using ax.add_feature()
-        #fig, ax = plt.subplots() # ... code to create the plot ...
-        #add_axis_elements.add_axis_elements(ax)
-        # plt.show()
     
     print(wrz.crs == outline.crs) # test if the crs is the same 
     # Append PCC for 2019 to 2020 to the wrz geodataframe
