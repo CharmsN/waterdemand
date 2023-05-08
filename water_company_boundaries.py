3a1935977f36192a9bd013cebafd1a89d49b0558
CharmsN <Newmarch-C@Ulster.ac.uk>
2023-05-08 12:01:55 +0100

    refining the code to make it more readable

--- water_company_boundaries.py
+++ water_company_boundaries.py
@@ -5,6 +5,27 @@ import cartopy.crs as ccrs
 
 def wrz_boundaries(company_data):
 
+    """
+    Generates a plot displaying the boundaries of water companies in England and Wales based on the provided company data.
+
+    The function reads the company data from a file, creates a GeoDataFrame using GeoPandas, and plots the boundaries of the water
+    companies on a map. Each company is represented by a different color. The resulting plot provides a visual representation of the
+    geographic distribution of water companies.
+    
+    The column to plot is hardcoded to COMPANY. A refinement to this code would be to create a variable for the volumn to plot.
+
+    Input:
+        company_data (str): The file path or name of the company data file. The file should contain the necessary information to
+        define the boundaries of water companies. Example is given in waterdemand Github repository 
+        (WaterSupplyAreas_incNAVs v1_4.shp)
+
+    Output:
+        plt: The plot object representing the boundaries of water companies in England and Wales.
+
+    Example:
+        company_data = 'path/to/company_data.shp'
+        wrz_boundaries(company_data)
+    """
 
     # Load water company data
     wrz = gpd.read_file(os.path.abspath(company_data))
