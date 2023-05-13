6d5315a3aafaf76d3e3ead85e89a4193fdf62388
CharmsN <Newmarch-C@Ulster.ac.uk>
2023-05-13 12:27:18 +0100

    Updating code to optimise

---
+++ correlation_landuse.py
@@ -0,0 +1,139 @@
+import os
+import pandas as pd
+import geopandas as gpd
+import pandas as pd
+import numpy as np
+from scipy.stats import pearsonr
+from shapely.ops import unary_union
+from functions import read_water_resource_zones
+
+"""
+This code performs data processing and analysis on water company data and land use data.
+
+1. Load water company data and filter it based on specific area types.
+2. Perform union operation on the geometries of water company groups.
+3. Create a new GeoDataFrame with the unioned geometries for each company.
+4. Append correlation data to the water company GeoDataFrame.
+5. Convert certain columns to numeric and filter out NaN values.
+6. Calculate the Pearson correlation coefficient between household population and consumption.
+7. Load land use data and merge it with the water company GeoDataFrame.
+8. Perform a spatial join between water companies and land use data.
+9. Group the data by company and land use label, and sum the area column.
+10. Create a new GeoDataFrame with the grouped data and centroid geometry.
+11. Filter the rows with 'urban' land use.
+12. Calculate the total area of urban land use for each company.
+13. Calculate the Pearson correlation coefficient between household consumption and urban land use area.
+
+The Pearson's Correlation Coefficient code is contributed by Amiya Rout (ref: https://www.geeksforgeeks.org/python-pearson-correlation-test-between-two-variables/)
+"""
+
+water_area_file = 'data_files/WaterSupplyAreas_incNAVs v1_4.shp'
+
+# Load water company data as wrz, remove unnecessary columns
+wrz = read_water_resource_zones(water_area_file)
+
+included_area_types = ['regional water and sewerage company', 'regional water only company'] # Filter the GeoDataFrame to select the features with "Northumbrian Water"
+
+wrz['COMPANY'] = wrz['COMPANY'].replace('Northumbrian Water Limited', 'Northumbrian Water') # fix issue with NES record
+
+# Filter out features with the specified area types
+wrz_ref = wrz[wrz['CoType'].isin(included_area_types)]
+ # len(wrz_ref) # uncomment this to check that all 43 water company areas are included 
+wrz_ref
+# Group the data by the 'Company' column
+grouped_wrz = wrz.groupby('COMPANY')
+
+# Create an empty GeoSeries to store the unioned geometries
+merged_geometries = gpd.GeoSeries()
+acronyms = []
+
+# Iterate over each group and perform the union operation
+for group_name, group_data in grouped_wrz:
+    unioned_geometry = group_data['geometry'].unary_union
+    merged_geometries[group_name] = unioned_geometry
+    acronyms.append(group_data['Acronym'].iloc[0])
+# Create a new GeoDataFrame with the unioned geometries for each company
+merged_wrz_companies = gpd.GeoDataFrame(geometry=merged_geometries.values, index=merged_geometries.index)
+merged_wrz_companies['COMPANY'] = merged_geometries.index
+merged_wrz_companies['Acronym'] = acronyms
+
+merged_wrz_companies
+# Append Correlation data to the wrz geodataframe
+# Load the CSV file
+correlation_data = pd.read_csv('data_files/correlation_data.csv', thousands=',')  
+correlation_data
+# Perform the merge
+correlate = merged_wrz_companies.merge(correlation_data[['Company', 'hh_cons', 'hh_pop']], how='left', left_on='Acronym', right_on='Company')
+
+correlate.to_file('data_files_correlate.shp')
+correlate
+# Convert the hh_pop and hh_cons columns to numeric
+correlate['hh_pop'] = pd.to_numeric(correlate['hh_pop'], errors='coerce')
+correlate['hh_cons'] = pd.to_numeric(correlate['hh_cons'], errors='coerce')
+
+# Filter out NaN values
+correlate = correlate.dropna()
+correlate
+# Convert dataframe into series
+list1 = correlate['hh_pop']
+list2 = correlate['hh_cons']
+
+# Apply the pearsonr()
+corr, _ = pearsonr(list1, list2)
+print('Pearsons correlation for household population and household consumption: %.3f' % corr)
+
+# This code is contributed by Amiya Rout (ref: https://www.geeksforgeeks.org/python-pearson-correlation-test-between-two-variables/)
+
+# Load landuse data
+landuse = gpd.read_file(os.path.abspath('data_files/clc2018_uk.shp'))
+# Load the CSV file
+landuse_categories = pd.read_csv('data_files/legend.csv')
+# print(landuse_categories.head())  #show a sample of the CSV file 
+# merge the csv file with the geodataframe to include the labels for the landuse in the geodataframe
+landuse_categories['CODE'] = landuse_categories['CODE'].astype(str)
+merged_landuse = pd.merge(landuse, landuse_categories, left_on='CODE_18', right_on='CODE')
+
+# Drop unnecessary columns - this cleans the dataset to make it easier to work with
+merged_landuse = merged_landuse.drop(['CODE_18', 'CODE', 'Unnamed: 4', 'Unnamed: 5'], axis=1)
+merged_landuse
+# Access the 'LABEL' column in the merged DataFrame - LABEL gives the actual landuse description
+label_column = merged_landuse['LABEL']
+merged_wrz_companies = merged_wrz_companies.set_crs(wrz.crs)
+# print(merged_landuse.crs == merged_wrz_companies.crs) # test if the crs is the same 
+# Perform spatial join between wrz and merged_landuse
+join = gpd.sjoin(merged_wrz_companies, merged_landuse, how='inner', predicate='intersects')
+# Group by COMPANY and LABEL, and sum the Area_Ha column
+grouped = join.groupby(['COMPANY', 'LABEL'])['Area_Ha'].sum().reset_index()
+
+# Create a new GeoDataFrame from the grouped data
+company_landuse = gpd.GeoDataFrame(grouped, geometry=gpd.GeoSeries(), crs=wrz.crs)
+
+# Set the geometry of the new GeoDataFrame to the centroid of each LABEL
+company_landuse.geometry = company_landuse.apply(lambda x: wrz[wrz['COMPANY'] == x['COMPANY']].geometry.centroid.iloc[0], axis=1)
+# Filter the rows where LABEL includes 'urban'
+urban_company_landuse = company_landuse[company_landuse['LABEL'].str.contains('urban')]
+urban_company_landuse
+# Group the rows by the COMPANY column and get the sum of the Area_Ha column for each group
+area_by_company = urban_company_landuse.groupby("COMPANY")["Area_Ha"].sum()
+
+# Round the values in the "Area_Ha" column to 2 significant digits
+area_by_company = area_by_company.round(2)
+
+# Convert the result to a new GeoDataFrame with a "COMPANY" column and an "AreaHa" column
+area_by_company = area_by_company.reset_index()
+area_by_company.columns = ["COMPANY", "Area_Ha"]
+area_by_company
+correlate
+# Perform the merge
+correlate_landuse = correlate.merge(area_by_company[['COMPANY', 'Area_Ha']], how='left', left_on='COMPANY', right_on='COMPANY')
+
+correlate_landuse
+# Convert dataframe into series
+list2 = correlate_landuse['hh_cons']
+list3 = correlate_landuse['Area_Ha']
+
+# Apply the pearsonr()
+corr, _ = pearsonr(list2, list3)
+print('Pearsons correlation for household population and area urban landuse: %.3f' % corr)
+
+
---
+++ download_sat_image_company.py
@@ -0,0 +1,105 @@
+import functions
+import geopandas as gpd
+from sentinelsat import SentinelAPI, make_path_filter
+from IPython import display
+import os
+
+def download_best_overlap_image(company_detail,date_start,date_end):
+    """
+    Downloads the best overlapping image for a given water company within a specified date range.
+
+    Inputs:
+        company_detail (str): The name of the water company. This is the AreaServed column in the dataset that is provided as example.
+        date_start (str): The start date of the date range in the format 'YYYYMMDD'.
+        date_end (str): The end date of the date range in the format 'YYYYMMDD'.
+
+    Outputs:
+        None
+    The code to download all matches must be uncommented if the results are to be downloaded.
+    """
+    
+    # Load water company data as wrz, remove unnecessary columns
+    wrz = gpd.read_file(os.path.abspath('data_files/WaterSupplyAreas_incNAVs v1_4.shp'))
+    
+    # select water company to review in further detail:
+    selected_company_gdf = wrz[wrz['AreaServed'] == company_detail]
+
+    # get the outline of the selected water company:
+    company_outline = selected_company_gdf['geometry']
+
+    # create a new GeoDataFrame with the outline of the selected water company:
+    outline_gdf = gpd.GeoDataFrame(geometry=company_outline)
+
+    # print the GeoDataFrame
+    print(outline_gdf)
+
+    # ensure that the crs for the the gdf of the water company selected is set to epsg 4326
+    outline_gdf = outline_gdf.to_crs(epsg=4326)
+
+    # convert the MULTIPOLYGON to a valid POLYGON
+    polygon = outline_gdf['geometry'].unary_union
+    #polygon # to visualise polygon
+
+    # get the minimum rotated angle 
+    search_area = polygon.minimum_rotated_rectangle
+
+    #search_area # to visual rotated rectangle
+
+    #connect to SentinelAPI
+    api = SentinelAPI(None, None, api_url='https://scihub.copernicus.eu/dhus') # connect to the SentinelAPi using sign on details on .netrc file
+
+    #retrieve images for 10% cloud cover, date range 1 April 2019 to 31 March 2020 
+    products = api.query(search_area.wkt, # use the WKT representation of our search area
+                     date=(date_start,date_end), # all images from February 2023
+                     platformname='Sentinel-2', # the platform name is Sentinel-2
+                     producttype='S2MSI2A', # surface reflectance product (L2A)
+                     cloudcoverpercentage=(0, 10)) # limit to 10% cloud cover
+    # Determine the number of images retrieved
+    nresults = len(products) # get the number of results found
+    print('Found {} results'.format(nresults)) # show the number of results found 
+    if nresults == 0:
+        print('No images in this range')   # this is to show that no results were found
+
+    results = next(iter(products)) # gets the second item from the dict
+    products[results] # show the metadata for the second item
+
+    qlook = api.download_quicklook(results) # download the quicklook image for this first result 
+    display.Image(qlook['path']) # display the image 
+    
+     # Retrieve the downloaded image path
+    qlook = api.download_quicklook(results)
+    image_path = qlook['path']
+
+    print("Image path:", image_path)  # Print the image path for debugging
+    
+    # Check if the image file exists
+    if os.path.exists(image_path):
+        # Display the image
+        display.Image(image_path)
+    else:
+        print('Image file not found at:', image_path)
+
+    product_geo = SentinelAPI.to_geodataframe(products) # convert the search results to a geodataframe
+    product_geo.head() # show the first 5 rows of the geodataframe
+
+    # calculate the % overlay of the image and the rectangle for selection
+    for ind, row in product_geo.iterrows():
+        intersection = search_area.intersection(row['geometry']) # find the intersection of the two polygons
+        product_geo.loc[ind, 'overlap'] = intersection.area / search_area.area # get the fractional overlap
+    
+    # print(product_geo.overlap) # show the fractional overlap for each index
+
+    # work out the max overlap and return the index
+    max_index = product_geo.overlap.argmax() # get the integer location of the largest overlap value
+    print(max_index) 
+    
+    best_overlap = product_geo.index[max_index] # get the actual index (image name) with the largest overlap
+    print(product_geo.loc[best_overlap]) # show the metadata for the image with the largest overlap
+
+    api.download(best_overlap) # downloads the first result
+    
+    #uncomment this section only if you want to download the results for all matches
+    #api.download_all(products,
+                 #n_concurrent_dl=5, # allow up to 5 concurrent downloads
+                 #nodefilter=make_path_filter("*_B*.jp2")) # only down the image bands (optional)
+    
